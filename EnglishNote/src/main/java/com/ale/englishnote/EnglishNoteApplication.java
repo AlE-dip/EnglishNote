d503ae5194f8ec47a04a7d58ffc86c7aef182640
Linh <71812422+AlE-dip@users.noreply.github.com>
2023-06-29 15:04:47 +0700

    create project

---
+++ EnglishNote/src/main/java/com/ale/englishnote/EnglishNoteApplication.java
@@ -0,0 +1,13 @@
+package com.ale.englishnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishNoteApplication.class, args);
+	}
+
+}
---
+++ EnglishNote/src/test/java/com/ale/englishnote/EnglishNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ale.englishnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
